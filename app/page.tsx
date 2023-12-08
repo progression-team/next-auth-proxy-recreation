9dace024e5ce00ca43878904926400f959b127d6
Sam Garson <samtgarson@gmail.com>
2023-12-08 14:53:01 +0000

    Preview commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import packageJSON from "../package.json"
 export default function Index() {
   return (
     <div className="space-y-2">
-      <h1 className="text-3xl font-bold">NextAuth.js Example</h1>
+      <h1 className="text-3xl font-bold">NextAuth.js Example (Preview)</h1>
       <p>
         This is an example site to demonstrate how to use{" "}
         <CustomLink href="https://nextjs.authjs.dev">NextAuth.js</CustomLink>{" "}
